5a0f8668c520a26d3d6ce4d1f6ccf2ac45b247af
Steven Woolston <steven.woolston@gmail.com>
2017-08-30 20:56:00 +1000

    First commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filesEntries = null;
+
+            string path = Directory.GetCurrentDirectory();
+            filesEntries = Directory.GetFiles(path);
+
+            PdfReader reader = null;
+            Document sourceDocument = null;
+            PdfCopy pdfCopyProvider = null;
+            PdfImportedPage importedPage;
+            string outputPdfPath = $@"{path.Replace("\\", "/")}/{"CombinedDocument.pdf"}";
+
+            sourceDocument = new Document();
+            pdfCopyProvider = new PdfCopy(sourceDocument, new System.IO.FileStream(outputPdfPath, System.IO.FileMode.Create));
+
+            //Open the output file
+            sourceDocument.Open();
+
+            try
+            {
+                //Loop through the files list
+                for (int f = 0; f < filesEntries.Length; f++)
+                {
+                    int pages = get_pageCcount(filesEntries[f]);
+
+                    reader = new PdfReader(filesEntries[f]);
+                    //Add pages of current file
+                    for (int i = 1; i <= pages; i++)
+                    {
+                        importedPage = pdfCopyProvider.GetImportedPage(reader, i);
+                        pdfCopyProvider.AddPage(importedPage);
+                    }
+
+                    reader.Close();
+                }
+                //At the end save the output file
+                sourceDocument.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static int get_pageCcount(string file)
+        {
+            using (StreamReader sr = new StreamReader(File.OpenRead(file)))
+            {
+                Regex regex = new Regex(@"/Type\s*/Page[^s]");
+                MatchCollection matches = regex.Matches(sr.ReadToEnd());
+
+                return matches.Count;
+            }
+        }
+    }
+}
