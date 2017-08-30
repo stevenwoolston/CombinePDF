1c69a69ce23c25b4cdf7997a9b448c08a1030353
Steven Woolston <steven.woolston@gmail.com>
2017-08-30 21:02:36 +1000

    Removed unnecessary references

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
